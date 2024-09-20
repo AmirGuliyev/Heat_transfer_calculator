26d8cdf7563bc81e3e3eca16e2d72a83ec98bc0b
AmirGuliyev <guliyevamir@yahoo.com>
2024-09-20 12:18:06 +0800

    Nothing

---
+++ Correlations.py
@@ -0,0 +1,136 @@
+import numpy as np
+
+class ExternalCorrelations:
+    def __init__(self, data, dim_par_data):
+        self.data = data
+        self.dim_par_data = dim_par_data
+
+    def lam_isothermal(self):
+
+        return self.data['flow_type'] == 'laminar' and self.data['plate_temp'] == "isothermal"
+
+    def turb_isothermal(self):
+
+        return self.data['flow_type'] == 'turbulent' and self.data['plate_temp'] == "isothermal"
+
+    def mix_isothermal(self):
+
+        return self.data['flow_type'] == 'mixed' and self.data['plate_temp'] == "isothermal"
+
+    def lam_unheated(self):
+
+        return self.data['flow_type'] == 'laminar' and self.data['plate_temp'] == 'unheated'
+
+    def turb_unheated(self):
+
+        return self.data['flow_type'] == 'turbulent' and self.data['plate_temp'] == 'unheated'
+
+    def delta_v(self):
+
+        if self.lam_isothermal():
+
+            return 5 * self.data['length'] / np.sqrt(self.dim_par_data['re'])
+
+        elif self.turb_isothermal():
+
+            return .37 * self.data['length'] * np.pow(self.dim_par_data['re'], -1/5)
+
+    def loc_cf(self):
+
+        if self.lam_isothermal():
+
+            return .664 / np.sqrt(self.dim_par_data['re'])
+
+        elif self.turb_isothermal():
+
+            return .0592 * np.pow(self.dim_par_data['re'], -1/5)
+
+    def loc_nu(self):
+
+        if self.dim_par_data['pr'] >= .6 and self.lam_isothermal():
+
+            return .332 * self.dim_par_data['re'] ** .5 * self.dim_par_data['pr'] ** (1/3)
+
+        elif self.dim_par_data['pr'] < .05 and 100 <= self.dim_par_data['pe'] and self.lam_isothermal():
+
+            return .565 * self.dim_par_data['pe'] ** .5
+
+        elif 100 <= self.dim_par_data['pe'] and self.dim_par_data['pr'] and self.lam_isothermal():
+
+            return .3387 * .5 * self.dim_par_data['re'] ** .5 * self.dim_par_data['pe'] ** (1/3) / (1 + (.0468 / self.dim_par_data['pe']) ** 2/3) ** .25
+
+        elif .6 <= self.dim_par_data['pr'] <= 60 and self.turb_isothermal():
+
+            return .0296 * self.dim_par_data['re'] ** 4/5 * self.dim_par_data['pr'] ** 1/3
+
+        elif self.lam_unheated():
+
+            return .332 * self.dim_par_data['re'] ** .5 * self.dim_par_data['pr'] ** (1/3) / ((1 - (self.data['xi'] / self.data['length']) ** 3/4) ** 1/3)
+
+        elif self.turb_unheated():
+
+            return .0296 * self.dim_par_data['re'] ** 4/5 * self.dim_par_data['pr'] ** 1/3 / ((1 - (self.data['xi'] / self.data['length']) ** 9/10) ** 1/9)
+
+    def loc_sh(self):
+
+        if .6 <= self.dim_par_data['sc'] and self.lam_isothermal():
+
+            return .332 * self.dim_par_data['re'] ** .5 * self.dim_par_data['sc'] ** (1/3)
+
+        elif .6 <= self.dim_par_data['sc'] <= 3000 and self.turb_isothermal():
+
+            return .0296 * self.dim_par_data['re'] ** 4 / 5 * self.dim_par_data['sc'] ** 1 / 3
+
+    def av_cf(self):
+
+        if self.lam_isothermal():
+
+            return 1.328 / np.sqrt(self.dim_par_data['re'])
+
+        elif self.dim_par_data['re'] <= 1e8 and self.mix_isothermal():
+
+            return .074 * np.pow(self.dim_par_data['re'], -1/5) - 2 * 871 / self.dim_par_data['re']
+
+        elif self.dim_par_data['re'] <= 1e8 and self.turb_isothermal():
+
+            return .074 * np.pow(self.dim_par_data['re'], -1/5) - 2 / self.dim_par_data['re']
+
+    def av_nu(self):
+
+        if 100 <= self.dim_par_data['pe']  and self.dim_par_data['pr'] and self.lam_isothermal():
+
+            return .3387 * self.dim_par_data['re'] ** .5 * self.dim_par_data['pe'] ** (1/3) / (1 + (.0468 / self.dim_par_data['pe']) ** 2/3) ** .25
+
+        elif .6 <= self.dim_par_data['pr'] and self.lam_isothermal():
+
+            return .664 * self.dim_par_data['re'] ** .5 * self.dim_par_data['pr'] ** (1/3)
+
+        elif .6 <= self.dim_par_data['pr'] <= 60 and self.turb_isothermal():
+
+            # CRITICAL REYNOLDS NUMBER IS 5e5
+
+            return (.037 * self.dim_par_data['re'] ** 4/5) * self.dim_par_data['pr'] ** 1/3
+
+        elif .6 <= self.dim_par_data['pr'] <= 60 and self.mix_isothermal():
+
+            # CRITICAL REYNOLDS NUMBER IS 5e5
+
+            return (.037 * self.dim_par_data['re'] ** 4/5 - 871) * self.dim_par_data['pr'] ** 1/3
+
+    def av_sh(self):
+
+        if .6 <= self.dim_par_data['sc'] and self.lam_isothermal():
+
+            return .664 * self.dim_par_data['re'] ** .5 * self.dim_par_data['sc'] ** (1/3)
+
+        elif .6 <= self.dim_par_data['sc'] <= 60 and self.turb_isothermal():
+
+            # CRITICAL REYNOLDS NUMBER IS 5e5
+
+            return (.037 * self.dim_par_data['re'] ** 4 / 5) * self.dim_par_data['sc'] ** 1 / 3
+
+        elif .6 <= self.dim_par_data['sc'] <= 60  and 5e5 <= self.dim_par_data['re'] <= 1e8 and self.mix_isothermal():
+
+            # CRITICAL REYNOLDS NUMBER IS 5e5
+
+            return (.037 * self.dim_par_data['re'] ** 4 / 5 - 871) * self.dim_par_data['sc'] ** 1 / 3
---
+++ DimPar.py
@@ -0,0 +1,273 @@
+"""     self.data.['h'] = h             # Heat transfer coefficient
+        self.data['length'] = length    # Characteristic length
+        self.data['ks'] = ks            # Solid heat conduction coefficient
+        self.data['hm'] = hm            # Mass transfer coefficient
+        self.data['d_ab'] = d_ab        # Binary diffusion coefficient
+        self.data['d'] = d              # Diameter
+        self.data['tau'] = g            # Gravitational acceleration constant
+        self.data['v'] = v              # Velocity
+        self.data['k'] = k              # Bulk modulus
+        self.data['cp'] = cp            # Const pressure specific heat capacity
+        self.data['t_sur'] = t_sur      # Surface temperature
+        self.data['t_sat'] = t_sat      # Saturation temperature
+        self.data['t_f'] = t_f          # Fluid temperature
+        self.data['alpha'] = alpha      # Thermal diffusivity
+        self.data['t'] = t              # Time
+        self.data['m_dot'] = m_dot      # Mass flux
+        self.data['tau'] = tau          # Surface shear stress
+        self.data['dp'] = dp            # Pressure Drop
+        self.data['rho'] = rho          # Density
+        self.data['d_rho'] = d_rho      # Vapor-liquid density difference
+        self.data['um'] = um            # Internal flow velocity
+        self.data['beta'] = beta        # Volumetric thermal expansion coefficient
+        self.data.f_lift = f_lift                   # Lift force
+        self.data['h_fg'] = h_fg                    # Latent energy during liquid-vapor phase change
+        self.data['kf'] = kf                        # Fluid heat conduction coefficient
+        self.data['nu'] = nu                        # Kinematic viscosity
+        self.data['mu'] = mu                        # Dynamic viscosity
+        self.data['sigma'] = sigma                  # Surface tension
+        self.data['flow_type'] = flow_type          # flow_type
+        self.data['int_low_geom'] = int_low_geom    # Internal flow geometry
+        self.data['delta_v'] = delta_v              # Velocity boundary layer
+        self.data['delta_t'] = delta_t              # Thermal boundary layer
+        self.data['delta_c'] = delta_c              # Concentration boundary layer"""
+
+import numpy as np
+
+
+class DimGro:
+    def __init__(self, data):
+        self.data = data
+
+    def re(self, numeric=False):
+        # Reynolds
+        if self.data['flowgeom'] == "external":
+            result = self.data['v'] * self.data['length'] / self.data['nu']
+            if result < 5e8:
+                if numeric:
+                    return result
+                return f"For external flow Re number is {result}. Flow is laminar"
+            elif 5e8 < result < 1e9:
+                if numeric:
+                    return result
+                return f"For external flow Re number is {result}. Flow is mixed"
+        else:
+            match self.data['int_low_geom']:
+                case "circular":
+                    result = 4 * self.data['m_dot'] / (np.pi * self.data['d'] * self.data['nu'])
+                    if numeric:
+                        return result
+                    return (f"For internal flow of circular geometry Re number is "
+                            f"{result}")
+                case _:
+                    result = self.data['v'] * self.data['d'] / self.data['nu']
+                    if numeric:
+                        return result
+                    return f"For internal flow Re number is {result}"
+
+    def pr(self, numeric=False):
+        # Prandtl
+        if self.data['cp'] is not None and self.data['mu'] is not None and self.data['kf'] is not None:
+            result = self.data['cp'] * self.data['mu'] / self.data['kf']
+            if numeric:
+                return result
+            return (f"Through specific heat, dynamic viscosity and fluid thermal conductivity "
+                    f"Pr number is {result}")
+        elif self.data['nu'] is not None and self.data['alpha'] is None:
+            result = self.data['nu'] / self.data['alpha']
+            if numeric:
+                return result
+            return (f"Through kinematic viscosity and thermal diffusivity "
+                    f"Pr number is {result}")
+        elif self.data['delta_v'] is not None and self.data['delta_t'] is None:
+            result = (self.data['delta_v'] / self.data['delta_t']) ** 3
+            if numeric:
+                return result
+            return (f"Through velocity boundary layer and thermal boundary layer "
+                    f"Pr number is {result}")
+
+    def sc(self, numeric = False):
+        # Schmidt
+        if self.data['nu'] is not None and self.data['d_ab'] is not None:
+            result = self.data['nu'] / self.data['d_ab']
+            if numeric:
+                return result
+            return (f"Through kinematic viscosity and binary diffusion coefficient "
+                    f"Sc number is {result}")
+        elif self.data['delta_v'] is not None and self.data['delta_c'] is None:
+            result = (self.data['delta_v'] / self.data['delta_c']) ** 3
+            if numeric:
+                return result
+            return (f"Through velocity boundary layer and thermal boundary layer "
+                    f"Sc number is {result}")
+    def st(self, numeric=False):
+        # Stanton
+        nusselt = self.nu()
+        reynold = self.re()
+        prandtl = self.pr()
+        if self.data['h'] is not None and self.data['rho'] is not None and self.data['v'] is not None and self.data['cp'] is not None:
+            result = self.data['h'] * self.data['rho'] * self.data['v'] * self.data['cp']
+            if numeric:
+                return result
+            return (f"Through heat transfer coefficient, fluid density, fluid velocity and constant pressure "
+                    f"specific heat capacity St number is {self.data['h'] / (self.data['rho'] * self.data['v'] * self.data['cp'])})")
+        elif nusselt is not None and reynold is not None and prandtl is not None:
+            result = nusselt / (reynold * prandtl)
+            if numeric:
+                return result
+            return f"Through Nu, Re and Pr dimensionless parameters St number is {nusselt / (reynold * prandtl)}"
+
+    def stm(self, numeric=False):
+        # Stanton mass
+        reynolds = self.re()
+        sherwood = self.sh()
+        schmidt = self.sc()
+        if self.data['hm'] is not None and self.data['v'] is not None:
+            result = self.data['hm'] / self.data['v']
+            if numeric:
+                return result
+            return result
+        elif sherwood is not None and schmidt is not None and reynolds is not None:
+            result = sherwood / (schmidt * reynolds)
+            if numeric:
+                return result
+            return f"Through Sh, Re and Sc dimensionless parameters Stanton(mass) number is {sherwood / (reynolds * schmidt)}"
+
+    def pe(self, numeric = False):
+        # Pecklet
+        reynolds = self.re()
+        prandtl = self.pr()
+        if self.data['v'] is not None and self.data['length'] is not None and self.data['alpha'] is not None:
+            result = self.data['v'] * self.data['length'] / self.data['alpha']
+            if numeric:
+                return result
+            return f"Through velocity, length and thermal diffusivity Pe number is {self.data['v'] * self.data['length'] / self.data['alpha']}"
+        elif reynolds is not None and prandtl is not None:
+            result = reynolds * prandtl
+            if numeric:
+                return result
+            return f"Through Re and Pr dimensionless parameters, Pe number is {reynolds * prandtl}"
+
+    def le(self, numeric = False):
+        # Lewis
+        schmidt = self.sc()
+        prandtl = self.pr()
+        if self.data['alpha'] is not None and self.data['d_ab'] is not None:
+            result = self.data['alpha'] / self.data['d_ab']
+            if numeric:
+                return result
+            return f"Through thermal diffusivity and binary diffusion coefficient Le number is {self.data['alpha'] / self.data['d_ab']}"
+        elif schmidt is not None and prandtl is not None:
+            if numeric:
+                return schmidt / prandtl
+            return f"Through Sc and Pr dimensionless parameters Le number is {schmidt / prandtl}"
+        elif self.data['delta_t'] is not None and self.data['delta_c'] is not None:
+            result = (self.data['delta_t'] / self.data['delta_c'])**3
+            if numeric:
+                return result
+            return (f"Through thermal boundary layer and concentration boundary layer "
+                    f"Pr number is {(self.data['delta_t'] / self.data['delta_c'])**3}")
+
+    def nu(self, numeric=False):
+        # Nusselt
+        reynolds = self.re()
+        prandtl = self.pr()
+        if self.data['h'] is not None and self.data['length'] is not None and self.data['kf'] is not None:
+            result = self.data['h'] * self.data['length'] * self.data['kf']
+            if numeric:
+                return result
+            return (f"Through heat convection coefficient, length and fluid's thermal conduction "
+                    f"Nu number is {self.data['h'] * self.data['length'] / self.data['kf']}")
+        elif reynolds is not None and prandtl is not None:
+            result = reynolds * prandtl
+            if numeric:
+                return result
+            return f"Through Re and Pr dimensionless parameters, Nu number is {result}"
+
+    def bi(self):
+        # Biot
+        return self.data['h'] * self.data['length'] / self.data['ks']
+
+    def bim(self):
+        # Biot mass
+        return self.data['hm'] * self.data['length'] / self.data['d_ab']
+
+    def bo(self):
+        # Bond
+        return self.data['tau'] * self.data['d_rho'] * self.data['length']**2 / self.data['hm']
+
+    def cf(self):
+        # Coef. of friction
+        return self.data['tau'] * 2 / (self.data['rho'] * self.data['v']**2)
+
+    def cp(self):
+        # Coef. of pressure
+        return self.data['dp'] * 2 / (self.data['rho'] * self.data['v']**2)
+
+    def cl(self):
+        # Coef. of lift
+        return 2 * self.data['length'] / (self.data['rho'] * self.data['v']**2 * self.data['f_lift'])
+
+    def ec(self):
+        # Eckert
+        return self.data['v']**2 / (self.data['cp'] * (self.data['t_sur'] - self.data['t_f']))
+
+    def fo(self):
+        # Fourier
+        return self.data['alpha'] * self.data['t'] / (self.data['length']**2)
+
+    def la(self):
+        # Laplace
+        return self.data['sigma'] * self.data['rho'] * self.data['length'] / (self.data['mu']**2)
+
+    def fr(self):
+        # Froude
+        return self.data['v'] / ((self.data['tau'] * self.data['length']) ** 0.5)
+
+    def eu(self):
+        # Euler
+        return self.data['dp'] * 2 / (self.data['rho'] * self.data['v']**2)
+
+    def fom(self):
+        # Fourier mass
+        return self.data['d_ab'] * self.data['t'] / self.data['length']**2
+
+    def f(self):
+        # Friction factor
+        return self.data['dp'] * self.data['d'] * 2 / (self.data['length'] * self.data['rho'] * self.data['um']**2)
+
+    def grl(self):
+        # Grashoff
+        return self.data['tau'] * self.data['beta'] * (self.data['t_sur'] - self.data['t_f']) * self.data['length']**3 / self.data['nu']
+
+    def coj(self):
+        # Colburn j factor
+        stanton = self.st(numeric=True)
+        prandtl = self.pr(numeric=True)
+        return stanton * prandtl**(2/3)
+
+    def cojm(self):
+        # Colburn j factor mass
+        stantonmass = self.stm(numeric=True)
+        schmidt = self.sc(numeric=True)
+        return stantonmass * schmidt ** (2/3)
+
+    def ja(self):
+        # Jakob
+        return self.data['cp'] * (self.data['t_sur'] - self.data['t_sat']) / self.data['h_fg']
+
+    def sh(self):
+        # Sherwood
+        return self.data['hm'] * self.data['length'] / self.data['d_ab']
+
+    def we(self):
+        # Weber
+        return self.data['rho'] * self.data['v']**2 * self.data['length'] / self.data['sigma']
+
+    def ra(self):
+        # Rayleigh
+        return self.data['tau'] * self.data['beta'] * (self.data['t_sur'] - self.data['t_f']) * self.data['length']**3 / (self.data['nu'] * self.data['alpha'])
+
+    def ca(self):
+        # Cauchy
+        return self.data['rho'] * self.data['v']**2 / self.data['k']
